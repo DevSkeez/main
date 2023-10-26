c53f3f483e0c3253ffe89b3bce805e377ab31750
Daniil <shred1@bk.ru>
2023-10-26 12:22:49 +0000

    Add Difference

--- sum.c
+++ sum.c
@@ -5,6 +5,8 @@ int main()
     int a = 8;
     int b = 12;
     int sum = a + b;
-    printf("Sum: %d\n", sum)
+    int dif = a - b;
+    printf("Sum: %d\n", sum);
+    printf("Dif: %d\n", dif);
     return 0;
 }
